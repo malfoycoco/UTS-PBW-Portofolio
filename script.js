186ef648747578ed9ff9997b15a522eaed6f110c
malfoycoco <malfoycoco@gmail.com>
2025-04-08 17:07:58 +0700

    Shofi Shulhiyana_2210631250069

---
+++ script.js
@@ -0,0 +1,7 @@
+// Untuk submit form (contoh)
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Pesan berhasil dikirim!");
+  });
+  
+  
